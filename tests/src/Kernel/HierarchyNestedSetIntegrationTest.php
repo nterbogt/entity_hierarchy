bc786730ea7335ca1c2f2b46eb1e9b21cfd087e6
Lee Rowlands <lee.rowlands@previousnext.com.au>
2017-02-03 15:24:10 +1000

    Add field schema

--- tests/src/Kernel/HierarchyNestedSetIntegrationTest.php
+++ tests/src/Kernel/HierarchyNestedSetIntegrationTest.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\Tests\entity_hierarchy\Kernel;
 
+use Drupal\Component\Utility\Unicode;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
@@ -23,6 +26,7 @@ class HierarchyNestedSetIntegrationTest extends KernelTestBase {
     'system',
     'user',
     'dbal',
+    'field',
   ];
 
   /**
@@ -32,8 +36,43 @@ class HierarchyNestedSetIntegrationTest extends KernelTestBase {
     parent::setUp();
   }
 
+  /**
+   * Tests storage in nested set tables.
+   */
   public function testNestedSetStorage() {
     $tree_storage = $this->container->get('entity_hierarchy.nested_set_storage_factory')->get(self::FIELD_NAME, self::ENTITY_TYPE);
+    $this->setupEntityHierarchyField('entity_test', 'entity_test', 'parent');
+  }
+
+  /**
+   * Creates a new entity hierarchy field for the given bundle.
+   *
+   * @param string $entity_type_id
+   *   Entity type to add the field to.
+   * @param string $bundle
+   *   Bundle of field.
+   * @param string $field_name
+   *   Field name.
+   */
+  protected function setupEntityHierarchyField($entity_type_id, $bundle, $field_name) {
+    $storage = FieldStorageConfig::create([
+      'entity_type' => $entity_type_id,
+      'field_name' => $field_name,
+      'id' => "$entity_type_id.$field_name",
+      'type' => 'entity_reference_hierarchy',
+      'settings' => [
+        'target_type' => $entity_type_id,
+      ],
+    ]);
+    $storage->save();
+    $config = FieldConfig::create([
+      'field_name' => $field_name,
+      'entity_type' => $entity_type_id,
+      'bundle' =>  $bundle,
+      'id' => "$entity_type_id.$bundle.$field_name",
+      'label' => Unicode::ucfirst($field_name),
+    ]);
+    $config->save();
   }
 
 }
