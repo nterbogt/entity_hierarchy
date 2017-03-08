41f6269fb9c032bde949bc398f0195835918b23d
Lee Rowlands <lee.rowlands@previousnext.com.au>
2017-03-08 11:19:42 +1000

    Issue #2858617 by larowlan: Add the ability to rebuild a nested set representation of the hierarchy: test

---
+++ tests/src/Kernel/RebuildTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\Tests\entity_hierarchy\Kernel;
+use PNX\NestedSet\Node;
+use PNX\NestedSet\NodeKey;
+
+/**
+ * Defines a class for testing rebuilding.
+ *
+ * @group entity_hierarchy
+ */
+class RebuildTest extends EntityHierarchyKernelTestBase {
+
+  public function testRebuild() {
+    $entities = $this->createChildEntities($this->parent->id());
+    $expected = [
+      new Node(new NodeKey('1', '1'), '1', '12', '0'),
+      new Node(new NodeKey('6', '6'), '2', '3', '1'),
+      new Node(new NodeKey('5', '5'), '4', '5', '1'),
+      new Node(new NodeKey('4', '4'), '6', '7', '1'),
+      new Node(new NodeKey('3', '3'), '8', '9', '1'),
+      new Node(new NodeKey('2', '2'), '10', '11', '1'),
+    ];
+    $this->assertEquals($expected, $this->treeStorage->getTree());
+    // Now mess with the database.
+    $this->container->get('database')->update('nested_set_parents_entity_test')
+      ->fields([
+        'left_pos' => 4,
+        'right_pos' => 5
+      ])
+      ->condition('id', 2)
+      ->execute();
+    $this->assertNotEquals($expected, $this->treeStorage->getTree());
+  }
+}
