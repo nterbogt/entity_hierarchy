f222fad73c78d12477335a08659668f7a0fc582a
Lee Rowlands <lee.rowlands@previousnext.com.au>
2017-08-24 20:03:03 +1000

    Issue #2904307 by larowlan: Reorder children saves every revision needlessly

--- tests/src/Functional/ReorderChildrenWithRevisionsFunctionalTest.php
+++ tests/src/Functional/ReorderChildrenWithRevisionsFunctionalTest.php
@@ -2,15 +2,36 @@
 
 namespace Drupal\Tests\entity_hierarchy\Functional;
 use Drupal\entity_test\Entity\EntityTestRev;
+use Drupal\simpletest\BlockCreationTrait;
+use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\entity_hierarchy\Traits\EntityHierarchyTestTrait;
+use PNX\NestedSet\Node;
 
 /**
  * Tests reordering with revisions.
  *
  * @group entity_hierarchy.
  */
-class ReorderChildrenWithRevisionsFunctionalTest extends ReorderChildrenFunctionalTest {
+class ReorderChildrenWithRevisionsFunctionalTest extends BrowserTestBase {
+
+  use EntityHierarchyTestTrait;
+  use BlockCreationTrait;
 
   const ENTITY_TYPE = 'entity_test_rev';
+  const FIELD_NAME = 'parents';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'entity_hierarchy',
+    'entity_test',
+    'system',
+    'user',
+    'dbal',
+    'block',
+    'field',
+  ];
 
   /**
    * {@inheritdoc}
@@ -53,8 +74,87 @@ class ReorderChildrenWithRevisionsFunctionalTest extends ReorderChildrenFunction
   /**
    * {@inheritdoc}
    */
-  public function testAddChildLinks() {
-    $this->assertTrue(TRUE, "We don't need to run this one for revisions.");
+  protected function setUp() {
+    parent::setUp();
+    $this->setupEntityHierarchyField(static::ENTITY_TYPE, static::ENTITY_TYPE, static::FIELD_NAME);
+    $this->additionalSetup();
+  }
+
+  /**
+   * Tests children reorder form.
+   */
+  public function testReordering() {
+    $entities = $this->createChildEntities($this->parent->id());
+    $root_node = $this->treeStorage->getNode($this->parentStub);
+    $children = $this->treeStorage->findChildren($root_node->getNodeKey());
+    $mapper = $this->container->get('entity_hierarchy.entity_tree_node_mapper');
+    $ancestors = $mapper->loadEntitiesForTreeNodesWithoutAccessChecks('entity_test_rev', $children);
+    $labels = $this->getLabels($ancestors);
+    $this->assertEquals([
+      'Child 5',
+      'Child 4',
+      'Child 3',
+      'Child 2',
+      'Child 1',
+    ], $labels);
+    // Now insert one in the middle.
+    $name = 'Child 6';
+    $entities[$name] = $this->createTestEntity($this->parent->id(), $name, -2);
+    $children = $this->treeStorage->findChildren($root_node->getNodeKey());
+    $ancestors = $mapper->loadEntitiesForTreeNodesWithoutAccessChecks('entity_test_rev', $children);
+    $labels = $this->getLabels($ancestors);
+    $this->assertEquals([
+      'Child 5',
+      'Child 4',
+      'Child 3',
+      'Child 2',
+      'Child 6',
+      'Child 1',
+    ], $labels);
+    // Now we visit the form for reordering.
+    $this->drupalLogin($this->drupalCreateUser([
+      'reorder entity_hierarchy children',
+      'view test entity',
+    ]));
+    $this->drupalGet($this->parent->toUrl('entity_hierarchy_reorder'));
+    $assert = $this->assertSession();
+    $assert->statusCodeEquals(200);
+    foreach ($entities as $entity) {
+      $assert->linkExists($entity->label());
+    }
+    // Now move Child 6 to the top.
+    $this->submitForm([
+      'children[' . $entities[$name]->id() . '][weight]' => -10,
+    ], 'Update child order');
+    $children = $this->treeStorage->findChildren($root_node->getNodeKey());
+    $ancestors = $mapper->loadEntitiesForTreeNodesWithoutAccessChecks('entity_test_rev', $children);
+    $labels = $this->getLabels($ancestors);
+    $this->assertEquals([
+      'Child 6',
+      'Child 5',
+      'Child 4',
+      'Child 3',
+      'Child 2',
+      'Child 1',
+    ], $labels);
+  }
+
+  /**
+   * Get labels.
+   *
+   * @param \SplObjectStorage $ancestors
+   *   Ancestors.
+   *
+   * @return array
+   *   Labels.
+   */
+  protected function getLabels(\SplObjectStorage $ancestors): array {
+    $labels = [];
+    foreach ($ancestors as $node) {
+      $entity = $ancestors->offsetGet($node);
+      $labels[] = $entity->label();
+    }
+    return $labels;
   }
 
 }
