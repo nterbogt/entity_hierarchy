ecb0157783b863a3a3dc4b539be73b1752c6579c
justanothermark <justanothermark@1369624.no-reply.drupal.org>
2017-10-19 07:50:33 +1000

    Issue #2915758 by justanothermark, larowlan: Add Views contextual filter for 'is Sibling of'

--- tests/src/Kernel/ViewsIntegrationTest.php
+++ tests/src/Kernel/ViewsIntegrationTest.php
@@ -162,4 +162,38 @@ class ViewsIntegrationTest extends EntityHierarchyKernelTestBase {
     $this->assertIdenticalResultset($executable, $expected, ['name' => 'name', 'id' => 'id']);
   }
 
+  /**
+   * Tests views sibling integration.
+   */
+  public function testViewsIntegrationSiblings() {
+    $children = $this->createChildEntities($this->parent->id(), 3);
+    $child = reset($children);
+    $this->createChildEntities($child->id(), 5);
+    // Tree is as follows
+    // 1     : Parent
+    // - 4   : Child 3
+    // - 3   : Child 2
+    // - 2   : Child 1
+    // - - 9 : Child 5
+    // - - 8 : Child 4
+    // - - 7 : Child 3
+    // - - 6 : Child 2
+    // - - 5 : Child 1
+    // Test showing single hierarchy.
+    $expected = [
+      [
+        'name' => 'Child 3',
+        'id' => 4,
+      ],
+      [
+        'name' => 'Child 2',
+        'id' => 3,
+      ],
+    ];
+    $executable = Views::getView('entity_hierarchy_test_children_view');
+    $executable->preview('block_4', [$child->id()]);
+    $this->assertCount(2, $executable->result);
+    $this->assertIdenticalResultset($executable, $expected, ['name' => 'name', 'id' => 'id']);
+  }
+
 }
