32d70d0b6393885dcc6f04815d04b64de256e3e2
fenstrat <fenstrat@362649.no-reply.drupal.org>
2020-07-07 08:09:50 +1000

    Issue #3156079 by fenstrat: Add api documentation for hook_entity_hierarchy_microsite_links_alter()

--- modules/entity_hierarchy_microsite/entity_hierarchy_microsite.api.php
+++ modules/entity_hierarchy_microsite/entity_hierarchy_microsite.api.php
@@ -20,3 +20,26 @@ function hook_entity_hierarchy_microsite_menu_item_url_alter(\Drupal\Core\Url $u
   $attributes['class'] = [$override->some_field->value];
   $url->setOption('attributes', $attributes);
 }
+
+/**
+ * Allows modules to alter the microsite menu links.
+ *
+ * @param array $links
+ *   The link definitions to be altered.
+ */
+function hook_entity_hierarchy_microsite_links_alter($links) {
+  // Disable all test node links in the microsite menu.
+  foreach ($links as $key => $link) {
+    if (empty($link['menu_name']) ||
+      $link['menu_name'] !== 'entity-hierarchy-microsite') {
+      continue;
+    }
+    if (empty($link['options']['entity']) ||
+      (!$node = $link['options']['entity']) ||
+      !$node instanceof \Drupal\node\NodeInterface ||
+      $node->bundle() !== 'test') {
+      continue;
+    }
+    $links[$key]['enabled'] = 0;
+  }
+}
