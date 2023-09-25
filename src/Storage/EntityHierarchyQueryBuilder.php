877540ec719f14daab195370037f7d2b6f5fd5b5
Nathan ter Bogt <nterbogt@gmail.com>
2023-09-25 13:40:05 +1000

    First pass at findAcestors

--- src/Storage/EntityHierarchyQueryBuilder.php
+++ src/Storage/EntityHierarchyQueryBuilder.php
@@ -7,6 +7,7 @@ use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\Sql\TableMappingInterface;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\migrate_drupal\Plugin\migrate\source\ContentEntity;
 use Psr\Log\LoggerInterface;
 
 class EntityHierarchyQueryBuilder {
@@ -31,7 +32,7 @@ class EntityHierarchyQueryBuilder {
   }
 
   public function findChildren(ContentEntityInterface $entity) {
-    // @todo Fix entity ID reference.
+    // @todo Fix entity_id reference.
     $column_weight = $this->getPropertyColumnName('weight');
     $result = $this->database->select($this->getTableName(), 'eh')
       ->fields('eh', ['entity_id', $column_weight])
@@ -44,6 +45,31 @@ class EntityHierarchyQueryBuilder {
       $children[$record->$column_weight] = $this->entityTypeManager->getStorage($this->fieldStorageDefinition->getTargetEntityTypeId())->load($record->entity_id);
     }
     return $children;
+  }
+
+  public function findAncestors(ContentEntityInterface $entity) {
+    $table_name = $this->getTableName();
+    $column_target_id = $this->getPropertyColumnName('target_id');
+    $column_entity_id = 'entity_id';
+    $sql = <<<CTESQL
+WITH RECURSIVE cte AS
+(
+  SELECT $column_entity_id, $column_target_id FROM {$table_name} WHERE entity_id = :entity_id
+  UNION ALL
+  SELECT c.$column_target_id, c.$column_target_id FROM {$table_name} c JOIN cte
+  ON c.$column_entity_id=cte.$column_target_id
+)
+SELECT * FROM cte;
+CTESQL;
+    $result = $this->database->query($sql, [
+      ':entity_id' => $entity->id()
+    ]);
+    $ancestors = [];
+    foreach ($result as $record) {
+      // @todo Optimise loading.
+      $ancestors[] = $this->entityTypeManager->getStorage($this->fieldStorageDefinition->getTargetEntityTypeId())->load($record->$column_target_id);
+    }
+    return array_reverse($ancestors);
   }
 
 }
